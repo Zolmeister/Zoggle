894448055f6145fdc90c872b273594cc5dbf0386
Zoli Kahan <zolikahan@gmail.com>
2014-01-02 20:14:55 -0600

    moved android app to simply be a wrapper for the zoggle website

--- android/platforms/android/bin/Zoggle-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/zoli/workspace/websites/zoggle2/android/platforms/android/bin/Zoggle-debug-unaligned.apk : \
-/home/zoli/workspace/websites/zoggle2/android/platforms/android/bin/Zoggle.ap_ \
-/home/zoli/workspace/websites/zoggle2/android/platforms/android/bin/classes.dex \
-/home/zoli/workspace/websites/zoggle2/android/platforms/android/CordovaLib/bin/classes.jar \
--- android/platforms/android/src/com/zolmeister/zoggle/Zoggle.java
+++ android/platforms/android/src/com/zolmeister/zoggle/Zoggle.java
@@ -30,7 +30,6 @@ public class Zoggle extends CordovaActivity
         super.onCreate(savedInstanceState);
         super.init();
         // Set by <content src="index.html" /> in config.xml
-        super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
     }
