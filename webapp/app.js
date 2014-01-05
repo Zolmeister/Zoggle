ea8513c800dcbecf4f79eb29c9d84c4b1153bdfb
Zoli Kahan <zolikahan@gmail.com>
2014-01-04 18:46:25 -0600

    testing new config for LTE workaround websockets, xhr-polling

--- webapp/app.js
+++ webapp/app.js
@@ -51,8 +51,7 @@ io.configure('production', function() {
   io.enable('browser client etag');          // apply etag caching logic based on version number
   io.enable('browser client gzip');          // gzip the file
   io.set('transports', [
-     'htmlfile'
-    , 'xhr-polling'
+     'xhr-polling'
     , 'jsonp-polling'
   ]);
 })
