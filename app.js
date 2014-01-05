10136f9fd97c2b88fd4353ba802a57f7834fc4af
Zoli Kahan <zolikahan@gmail.com>
2014-01-04 18:54:46 -0600

    Revert "testing new config for LTE workaround websockets, xhr-polling"
    This reverts commit ea8513c800dcbecf4f79eb29c9d84c4b1153bdfb.

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ io.configure('production', function() {
   io.enable('browser client etag');          // apply etag caching logic based on version number
   io.enable('browser client gzip');          // gzip the file
   io.set('transports', [
-     'xhr-polling'
+     'htmlfile'
+    , 'xhr-polling'
     , 'jsonp-polling'
   ]);
 })
